4f45a6e0d0949adcd68c79f59dd93b832316c646
Becca Byrne <aggiebec06@gmail.com>
2021-10-18 18:33:17 -0500

    added reset feature to entry boxes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,16 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
 
     // package up data as an object 
     var taskDataObj = {
@@ -30,6 +37,8 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.innerHTML = "<h3 class='task-name'>" +taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
     
+    console.dir(listItemEl);
+
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
